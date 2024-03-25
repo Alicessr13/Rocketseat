26ee3da7a2e89096925bee1f909b93fb1a0dbf62
Alice Stephanie Soares Ribeiro <alicessr12@gmail.com>
2024-03-24 22:27:35 -0300

    .

--- Explorer/JavaScript/Algoritmos e lógica de programação/script.js
+++ Explorer/JavaScript/Algoritmos e lógica de programação/script.js
@@ -7,6 +7,7 @@ let nome = prompt("Qual o seu nome?") //abre uma caixa com um campo de pergunta
 alert("Ola "+nome) //função que abre uma caixa no navegador
 */
 
+/*
 let num1 = prompt('Primeiro número: ')
 let num2 = prompt('Segundo número: ')
 
@@ -14,3 +15,22 @@ let result = (Number(num1) + Number(num2))/2
 
 alert("Resultado final: "+result);
 //alert("Resultado final: "+ ( (Number(num1) + Number(num2))/2 ) )
+*/
+
+let num1 = prompt('Digite o primeiro número: ')
+let num2 = prompt('Digite o segundo número: ')
+
+num1 = Number(num1)//leitura <- le o num1 transforma em number e atribui a num1 novamente
+num2 = Number(num2)
+
+const sum = num1 + num2
+const sub = num1 - num2
+const multi = num1 * num2
+const div = num1 /num2
+const restDiv = num1 % num2
+
+alert('Soma: '+sum);
+alert('Subtração: '+sub);
+alert('Multiplicação: '+multi);
+alert('Divisão: '+div);
+alert('Resto divisão: '+restDiv);
