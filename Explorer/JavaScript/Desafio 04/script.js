9bddc367be2114f24578df7b438499c771d6fadf
Alice Stephanie Soares Ribeiro <alicessr12@gmail.com>
2024-04-02 17:54:04 -0300

    exercício 04

--- Explorer/JavaScript/Desafio 04/script.js
+++ Explorer/JavaScript/Desafio 04/script.js
@@ -4,15 +4,33 @@
 //alert("Hello World!")
 
 //2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
-let num1 = prompt('Variavel 1: ')
+/*let num1 = prompt('Variavel 1: ')
 let num2 = prompt('Variavel 2: ')
 
 let soma = Number(num1) + Number(num2)
 
 alert(`O resultado da soma é: ${soma}`)
+*/
 
-//3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".   
+//3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".  
+/*let variavel = 10
+
+if(typeof(variavel) === 'number'){
+    alert('É um número')
+}else{
+    alert('Não é um número')
+}
+*/
+
 //4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+let variavel = 'a'
+
+if(typeof(variavel) === "string"){
+    alert('É uma string')
+}else{
+    alert('Não é um uma string')
+}
+
 //5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
 //6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
 //7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
