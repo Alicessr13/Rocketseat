cb04f1874ef8e46125dbec952e6ac82a208302ac
Alice Stephanie Soares Ribeiro <alicessr12@gmail.com>
2024-04-28 00:50:14 -0300

    funções, modal

--- Explorer/JavaScript/imc/script.js
+++ Explorer/JavaScript/imc/script.js
@@ -3,6 +3,27 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
+//const modalWrapper = document.querySelector('.modal-wrapper')
+//const modalMessage = document.querySelector('.modal .title span')
+//const modalBtnClose = document.querySelector('.modal button.close')
+
+const Modal = {
+
+    wrapper: document.querySelector('.modal-wrapper'),
+
+    message: document.querySelector('.modal .title span'),
+
+    btnClose: document.querySelector('.modal button.close'),
+
+    open: function(){
+        Modal.wrapper.classList.add('open')
+    },
+    close(){
+       Modal.wrapper.classList.remove('open')
+    }
+}
+
+
 form.onsubmit = function(event) { /**função anonima */
 /**função só vai ser executada ao clicar no button submit */
 /**padrão de um form submit é recarregar a página ao clicar*/
@@ -11,8 +32,26 @@ form.onsubmit = function(event) { /**função anonima */
 
     const weight = inputWeight.value
     const height = inputHeight.value
+
+    const result = IMC(weight,height)
+
+    const message = `Seu imc é de ${result}`
+
+    Modal.message.innerText = message
+
+    //modalWrapper.classList.add('open')
+    Modal.open()
 }
 
+modal.btnClose.onclick = () => {
+    //modalWrapper.classList.remove('open')
+    Modal.close()
+}
+
+
+function IMC(weight, height){
+    return (weight / ( (height / 100) **2) ).toFixed(2)
+}
 
 /**
  * 
