c485e7691676780084cade38b3869b4abe333245
Alice Stephanie Soares Ribeiro <alicessr12@gmail.com>
2024-04-29 23:54:37 -0300

    import continuação

--- Explorer/JavaScript/ESmodules/main.js
+++ Explorer/JavaScript/ESmodules/main.js
@@ -1,1 +1,32 @@
-import './utils.js' // nessa linha ele importa e já executa o arquivo
+//import './utils.js' // nessa linha ele importa e já executa o arquivo
+
+import qualquerNome from "./utils.js"; //importa com qualquer nome
+console.log(qualquerNome) //imprime o que foi exportado
+
+import { today } from "./utils.js";
+console.log(today)
+
+import { double } from "./utils.js";
+console.log(double(3))
+
+//import{today, double} from "./utils.js" importa mais de um
+
+import funcaoPadrao, {date, greeting} from './utils.js'
+
+console.log(date)
+
+console.log(greeting('Alice'))
+
+funcaoPadrao('Alice')
+
+import {sum as soma} from './utils.js' //importa sum com o nome de soma
+console.log(soma(3,4))
+
+
+//export const sum = (a, b) => a + b
+//export const multiply = (a, b) => a * b
+//export const subtract = (a, b) => a - b
+//export const divide = (a, b) => a / b
+
+// ou
+// export { sum, multiply, subtract, divide }
--- Explorer/JavaScript/ESmodules/utils.js
+++ Explorer/JavaScript/ESmodules/utils.js
@@ -4,3 +4,34 @@
 const message = "mensagem" // essa variável fica isolada nesse arquivo, 
 //e não consigo usar ela em outro lugar
 //alert(message) //o alerta vai aparecer mas não consigo utilizar a variavel mensagem
+
+//export default message //exportando a mensagem para ser acessivel em outro arquivo, default pode ser qualquer nome no import
+
+//somente tem um default por arquivo
+
+//para outras variaveis ou função precisa ser o mesmo nome
+export const today = Date.now();
+
+const double = number => number * 2;
+export {double}
+
+//export {today, double} exporta mais de 1
+
+const date = new Date()
+
+const greeting = name => `Hello ${name}`
+
+export default function (argument) {
+    console.log(date, greeting(argument));
+}
+
+export {date, greeting}
+
+
+export function sum(a, b){
+    return a + b
+}
+
+//import * as calculator from './utils.js' //importa todos os named exports e dar um nome padrão.
+//console.log(calculator.divide(49,4))
+
