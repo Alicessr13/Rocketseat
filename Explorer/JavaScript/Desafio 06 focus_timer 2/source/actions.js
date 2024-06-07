52e7739b7d4aafb7781635c05b7ee2a771c14d8a
Alice <alicessr12@gmail.com>
2024-06-06 23:13:13 -0300

    ajustes

---
+++ Explorer/JavaScript/Desafio 06 focus_timer 2/source/actions.js
@@ -0,0 +1,18 @@
+import state from "./state.js"
+
+//nomes das funções são os nomes que estão no data-action no html
+
+export function toggleRunning(){
+    state.isRunning = document.documentElement.classList.toggle('running')
+    //toggle adiciona ou tira a classe renning (se tiver tira, se não tiver adiciona) 
+    //pois o toggle returna true ou false
+    //começa em false
+}
+
+const minutes = document.getElementById('temporizador')
+
+
+export function set(){
+    minutes.setAttribute('contenteditable', true)
+    minutes.focus()
+}
--- Explorer/JavaScript/Desafio 06 focus_timer 2/source/elements.js
+++ Explorer/JavaScript/Desafio 06 focus_timer 2/source/elements.js
@@ -1,7 +1,26 @@
+
+
+import * as actions from "./actions.js"
+
 export const controls = document.getElementById('controls')
 
 export function registerControls(){
     controls.addEventListener('click',(event) => {
+        ////obsrva os clicks dos elementos do controle (controls)
+        
         console.log('clicou em controls')
+
+        const action = event.target.dataset.action
+        //essa linha de código está obtendo o valor do atributo data-action do elemento que disparou o evento e o armazenando na constante action.
+        //event.target = evento alvo
+
+        if( typeof actions[action]  != "function"){ 
+            //se o data-action capturar algo que não seja uma função existente (undefined, erro) para a função
+            return 
+        }
+       
+        actions[action]()
+         //action recebe o nome do click (dataset.action) que é o mesmo nome da função em actions, por isso actions executa a função correta
     })
 }
+
